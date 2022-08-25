04d3e84018d6cf0b7bd3c1c268f69391737ec197
전일훈 <ktdvlphun@gmail.com>
2022-08-26 02:21:53 +0900

    test

--- src/main/java/com/ggurys/springbootwebservice/Application.java
+++ src/main/java/com/ggurys/springbootwebservice/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // JPA Auditing 활성화 
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class Application {
 
